979ce5b2f07e8db94a7800f602669af8601e247d
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-28 22:58:43 +0500

    Date Methods

---
+++ script.js
@@ -0,0 +1,11 @@
+//  Question 1 
+
+ document.write( "<h1>" + "Question 1" + "<br>"  + "</h1>");
+
+ var date = new Date();
+
+ document.write(date);
+
+//  Question 2
+
+ document.write( "<h1>" + "Question 2" + "<br>"  + "</h1>");
