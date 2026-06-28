e8785111b8c6ff2d39fd8f3a52a5d51fa81e96d0
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-28 23:07:05 +0500

    Date Methods

--- script.js
+++ script.js
@@ -9,3 +9,21 @@
 //  Question 2
 
  document.write( "<h1>" + "Question 2" + "<br>"  + "</h1>");
+
+var currentMonth = date.getMonth();
+var months = [
+    "January" , 
+    "February", 
+    "March", "April"
+     ,"May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ]
+
+    alert("Current Month: " + months[currentMonth]);
+    document.write("ALert Message");
